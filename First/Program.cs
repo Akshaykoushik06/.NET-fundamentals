9201fb87f59201e5287c2bc249dde47dab2aa792
Akshaykoushik06 <akshaykoushikhothur@gmail.com>
2021-07-26 20:46:33 +0530

    initial commit

---
+++ First/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your name");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
